e06c1a8f9f54c2118712f6df7c63b6e3a9c05b5a
OverdriveJon <jvoliveiraresende@hotmail.com>
2022-01-06 17:11:23 -0300

    Refactor code
    -Pasta Interfaces
    -Pasta Providers
    -Pasta Struct
    Codigo semi-refeito

---
+++ SpaceCommander/Assets/Scripts/MouseScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    Vector3 m_mousePos;
+    public Camera camera;
+    public Transform currentTarget;
+    
+    Ray ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(currentTarget);
+        m_mousePos = Input.mousePosition;
+        
+              
+    }
+
+	private void FixedUpdate()
+	{
+        Hoover();
+    }
+
+	void Hoover()
+	{
+        RaycastHit hit;
+        ray = camera.ScreenPointToRay(m_mousePos);
+     
+        if (currentTarget != null)
+        {
+            
+            currentTarget.gameObject.layer = LayerMask.NameToLayer("Planet");
+            currentTarget = null;
+        }
+
+        if (Physics.Raycast(ray, out hit, 2000.0f, LayerMask.GetMask("Planet")))
+        {
+            var target = hit.transform;
+            currentTarget = target;
+            currentTarget.gameObject.layer = LayerMask.NameToLayer("Hoovered");
+            Debug.Log(currentTarget);
+        }
+    }
+}
---
+++ SpaceCommander/Assets/Scripts/Structs/MovableBody.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public struct MovableBody
+{
+    public Transform Transform;
+    public float Speed;
+
+	public MovableBody(Transform transform, float speed)
+	{
+        Transform = transform;
+		Speed = speed;
+	}
+}
